17020d286bacfbf334de33dddffecc40b507ce43
Dr.-Ing. Sebastian Hahner <hi@sebinside.de>
2026-01-27 16:53:39 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -39,6 +39,7 @@ alias v='code .'
 alias d='docker'
 alias dl="docker ps --format '{{printf \"\x1b[94m%s\x1b[0m\" .ID}}\t{{printf \"%.20s\" .Image}}\t{{printf \"\x1b[32m%s\x1b[0m\" .Status}}\t{{.Names}}'"
 alias n='npm run'
+alias cpb='git branch | grep "*" | sed "s/\* //g" | xclip -selection clipboard'
 
 # de = docker exec -it ... bash
 de() {
